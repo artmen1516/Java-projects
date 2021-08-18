babf5553e532a446c4092ec41fa07a7cba5a4f62
Arturo Mendivil <j.mendivil@samsung.com>
2021-08-18 11:14:15 -0700

    Deleted serial-terminal folder, added open serial and send serial

--- src/Terminal.java
+++ src/Terminal.java
@@ -4,10 +4,15 @@
  * and open the template in the editor.
  */
 
-import com.formdev.flatlaf.FlatDarkLaf;
+//import com.formdev.flatlaf.FlatDarkLaf;
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime;   
 import com.fazecast.jSerialComm.SerialPort;
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import java.io.OutputStream;
+import java.io.IOException;
 
 /**
  *
@@ -16,6 +21,7 @@ import com.fazecast.jSerialComm.SerialPort;
 public class Terminal extends javax.swing.JFrame {
 
     SerialPort SerialPort1;
+    OutputStream outputStream1;
     /**
      * Creates new form Terminal
      */
@@ -51,8 +57,11 @@ public class Terminal extends javax.swing.JFrame {
         jComboBox_parity = new javax.swing.JComboBox<>();
         jComboBox_dataBits = new javax.swing.JComboBox<>();
         jComboBox_stopBits = new javax.swing.JComboBox<>();
-        jComboBox_endOfLine = new javax.swing.JComboBox<>();
         jToggleButton_open = new javax.swing.JToggleButton();
+        jLabel_appOpenedDT = new javax.swing.JLabel();
+        jLabel_currentPort = new javax.swing.JLabel();
+        jTextField_stx = new javax.swing.JTextField();
+        jTextField_etx = new javax.swing.JTextField();
 
         jLabel1.setText("jLabel1");
 
@@ -106,6 +115,7 @@ public class Terminal extends javax.swing.JFrame {
         jLabel8.setText("Stop bits:");
 
         jComboBox_comPort.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "        " }));
+        jComboBox_comPort.setSelectedIndex(-1);
         jComboBox_comPort.addPopupMenuListener(new javax.swing.event.PopupMenuListener() {
             public void popupMenuCanceled(javax.swing.event.PopupMenuEvent evt) {
             }
@@ -124,16 +134,13 @@ public class Terminal extends javax.swing.JFrame {
         jComboBox_baudRate.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200" }));
         jComboBox_baudRate.setSelectedIndex(3);
 
-        jComboBox_parity.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "None", "Odd", "Even", "Mark", "Space" }));
+        jComboBox_parity.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "NONE_PARITY", "ODD_PARITY", "EVEN_PARITY", "MARK_PARITY", "SPACE_PARITY" }));
 
         jComboBox_dataBits.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "7", "8", "9" }));
         jComboBox_dataBits.setSelectedIndex(1);
 
-        jComboBox_stopBits.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "None", "One", "Two" }));
-
-        jComboBox_endOfLine.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "None", "\\r\\n" }));
-        jComboBox_endOfLine.setSelectedIndex(1);
-        jComboBox_endOfLine.setMinimumSize(new java.awt.Dimension(57, 20));
+        jComboBox_stopBits.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "0", "1", "2" }));
+        jComboBox_stopBits.setSelectedIndex(1);
 
         jToggleButton_open.setText("Open");
         jToggleButton_open.addActionListener(new java.awt.event.ActionListener() {
@@ -142,13 +149,29 @@ public class Terminal extends javax.swing.JFrame {
             }
         });
 
+        jLabel_currentPort.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+
+        jTextField_stx.setText("02");
+        jTextField_stx.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField_stxActionPerformed(evt);
+            }
+        });
+
+        jTextField_etx.setText("03");
+        jTextField_etx.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField_etxActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -157,23 +180,29 @@ public class Terminal extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jComboBox_comPort, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jToggleButton_open, javax.swing.GroupLayout.DEFAULT_SIZE, 95, Short.MAX_VALUE))
+                                    .addComponent(jToggleButton_open, javax.swing.GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jLabel3)
+                                        .addGap(0, 0, Short.MAX_VALUE)))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton_send, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextField_sendData, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jButton_send, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+                                    .addComponent(jTextField_sendData))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton_clear, javax.swing.GroupLayout.DEFAULT_SIZE, 77, Short.MAX_VALUE)
-                                    .addComponent(jComboBox_endOfLine, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                    .addComponent(jButton_clear, javax.swing.GroupLayout.DEFAULT_SIZE, 78, Short.MAX_VALUE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jTextField_stx, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(jTextField_etx))))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
                                         .addComponent(jComboBox_baudRate, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(jComboBox_parity, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addComponent(jComboBox_parity, 0, 102, Short.MAX_VALUE))
                                     .addGroup(layout.createSequentialGroup()
                                         .addComponent(jLabel5)
                                         .addGap(18, 18, 18)
@@ -189,26 +218,31 @@ public class Terminal extends javax.swing.JFrame {
                                         .addComponent(jLabel8)
                                         .addGap(0, 0, Short.MAX_VALUE))
                                     .addComponent(jComboBox_stopBits, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-                    .addGroup(layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addGap(15, 15, 15)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jScrollPane1)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addGap(0, 0, Short.MAX_VALUE)))))
+                                .addComponent(jLabel_appOpenedDT, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel_currentPort, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel_appOpenedDT, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel_currentPort, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jComboBox_endOfLine, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jTextField_sendData, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField_sendData, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField_stx, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField_etx, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton_clear)
@@ -240,7 +274,18 @@ public class Terminal extends javax.swing.JFrame {
 
     private void jButton_sendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_sendActionPerformed
         // TODO add your handling code here:
+        outputStream1 = SerialPort1.getOutputStream();
+        String dataToSend;
         
+        dataToSend = jTextField_stx.getText()+jTextField_sendData.getText()+jTextField_etx.getText();
+
+        try{
+            outputStream1.write(jTextField_stx);
+            outputStream1.write(dataToSend.getBytes());
+        }
+        catch(IOException c){
+            JOptionPane.showMessageDialog(this,c,"ERROR",ERROR_MESSAGE);
+        }
         
     }//GEN-LAST:event_jButton_sendActionPerformed
 
@@ -252,12 +297,14 @@ public class Terminal extends javax.swing.JFrame {
         // TODO add your handling code here:
         jButton_send.setEnabled(false);
         jTextField_sendData.setEnabled(false);
-        jComboBox_endOfLine.setEnabled(false);
+        jTextField_stx.setEnabled(false);
+        jTextField_etx.setEnabled(false);
+        //jComboBox_endOfLine.setEnabled(false);
         jButton_clear.setEnabled(false);
         
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
-        jTextArea_allData.setText("Application started at " + dtf.format(now) + "\r\n");
+        jLabel_appOpenedDT.setText("Application started at " + dtf.format(now) + "\r\n");
         
         
     }//GEN-LAST:event_formWindowOpened
@@ -279,19 +326,71 @@ public class Terminal extends javax.swing.JFrame {
         // TODO add your handling code here:
         if(jToggleButton_open.isSelected() == true){
             try{
-                /*SerialPort []portlist = SerialPort.getCommPorts();
+                SerialPort []portlist = SerialPort.getCommPorts();
                 SerialPort1 = portlist[jComboBox_comPort.getSelectedIndex()];
-                SerialPort1.setBaudRate(jComboBox_baudRate)*/
-                jToggleButton_open.setText("Close");
+                SerialPort1.setBaudRate(Integer.parseInt(jComboBox_baudRate.getSelectedItem().toString()));
+                SerialPort1.setParity(jComboBox_parity.getSelectedIndex());
+                SerialPort1.setNumDataBits(Integer.parseInt(jComboBox_dataBits.getSelectedItem().toString()));
+                SerialPort1.setNumStopBits(Integer.parseInt(jComboBox_stopBits.getSelectedItem().toString()));
+                SerialPort1.openPort();
+                
+                if(SerialPort1.isOpen()){
+                    jButton_send.setEnabled(true);
+                    jTextField_sendData.setEnabled(true);
+                    jTextField_stx.setEnabled(true);
+                    jTextField_etx.setEnabled(true);
+                    //jComboBox_endOfLine.setEnabled(true);
+                    jButton_clear.setEnabled(true);
+                    
+                    jComboBox_comPort.setEnabled(false);
+                    jComboBox_baudRate.setEnabled(false);
+                    jComboBox_parity.setEnabled(false);
+                    jComboBox_dataBits.setEnabled(false);
+                    jComboBox_stopBits.setEnabled(false);
+                    jToggleButton_open.setText("Close");
+                    jLabel_currentPort.setText(jComboBox_comPort.getSelectedItem().toString() + " Opened ");
+                    jLabel_currentPort.setForeground(Color.red);
+                }
+                else{
+                    JOptionPane.showMessageDialog(this,"ERROR TO OPEN " + jComboBox_comPort.getSelectedItem().toString(),"ERROR",ERROR_MESSAGE);
+                    jToggleButton_open.setSelected(false);
+                }
             }
-            catch(Exception e){
-            
+            catch(ArrayIndexOutOfBoundsException a){
+                JOptionPane.showMessageDialog(this,"SELECT A COM PORT!","ERROR",ERROR_MESSAGE);
+                jToggleButton_open.setSelected(false);
+            }
+            catch(Exception b){
+                JOptionPane.showMessageDialog(this,b,"ERROR",ERROR_MESSAGE);
+                jToggleButton_open.setSelected(false);
             }
         } 
         else if(jToggleButton_open.isSelected() == false){
+            SerialPort1.closePort();
+            jButton_send.setEnabled(false);
+            jTextField_sendData.setEnabled(false);
+            jTextField_stx.setEnabled(false);
+            jTextField_etx.setEnabled(false);
+            //jComboBox_endOfLine.setEnabled(false);
+            jButton_clear.setEnabled(false);
+        
+            jComboBox_comPort.setEnabled(true);
+            jComboBox_baudRate.setEnabled(true);
+            jComboBox_parity.setEnabled(true);
+            jComboBox_dataBits.setEnabled(true);
+            jComboBox_stopBits.setEnabled(true);
             jToggleButton_open.setText("Open");
+            jLabel_currentPort.setText("");
         }
     }//GEN-LAST:event_jToggleButton_openActionPerformed
+
+    private void jTextField_stxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField_stxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField_stxActionPerformed
+
+    private void jTextField_etxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField_etxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField_etxActionPerformed
 
     /**
      * @param args the command line arguments
@@ -334,7 +433,6 @@ public class Terminal extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> jComboBox_baudRate;
     private javax.swing.JComboBox<String> jComboBox_comPort;
     private javax.swing.JComboBox<String> jComboBox_dataBits;
-    private javax.swing.JComboBox<String> jComboBox_endOfLine;
     private javax.swing.JComboBox<String> jComboBox_parity;
     private javax.swing.JComboBox<String> jComboBox_stopBits;
     private javax.swing.JLabel jLabel1;
@@ -345,9 +443,13 @@ public class Terminal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel_appOpenedDT;
+    private javax.swing.JLabel jLabel_currentPort;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea_allData;
+    private javax.swing.JTextField jTextField_etx;
     private javax.swing.JTextField jTextField_sendData;
+    private javax.swing.JTextField jTextField_stx;
     private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JToggleButton jToggleButton_open;
     // End of variables declaration//GEN-END:variables
